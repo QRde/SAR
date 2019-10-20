f6ad8b0f4b719df88fcd8de10635984c6974966d
QRde <55965160+QRde@users.noreply.github.com>
2019-10-20 11:44:40 +0900

    Update genie.js

--- genie.js
+++ genie.js
@@ -159,9 +159,9 @@ function WakeupGenie() {
     el.id = 'genie-block';
     el.setAttribute('class', 'inline-block_test');
     var buf = '<button style="background-color:#e0e0ff" onclick="toggleQR()">QR</button>'
-         + '<input id="genie" size="50" style="background-color:#e0e0ff" placehoder="DnD or direct JS-code"></input>';
+         + '<input id="genie" size="50" style="background-color:#e0e0ff" placehoder="DnD or direct JS-code"></input>'
 		 + '<input type="button" onClick="recognition.start()" value="音認">音認</input>'
-		 + '<input type="button" onClick="recognition.stop()" value="停止">停止</input>'
+		 + '<input type="button" onClick="recognition.stop()" value="停止">停止</input>';
     el.innerHTML = buf;
     d.body.insertBefore(el, d.body.firstChild);
 
